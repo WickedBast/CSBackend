0f26d09fae5c22a57d6c3dd8c60565c6cd008f9e
Fabian <frzadkowski@wp.pl>
2022-03-09 13:27:58 -0800

    Update settings.py

--- CSBackend/settings.py
+++ CSBackend/settings.py
@@ -28,13 +28,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 
     # LIBRARY
     'rest_framework',
     'rest_framework.authtoken',
     'django_rest_passwordreset',
     'drf_yasg',
-    'django_filters',
 
     # LOCAL
     'users',
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
