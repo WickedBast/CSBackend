9b7afda460b126ee92641f9c86b771ed52cfaec6
WickedBast <ogul.tutuncu@isik.edu.tr>
2022-03-23 10:36:22 +0000

    .env File added

--- CSBackend/settings.py
+++ CSBackend/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.38.139.134"]
 
 # Application definition
 
