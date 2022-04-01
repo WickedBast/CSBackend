e9e2948b7a95490a9da35400de8c0053bf0c3362
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2022-04-01 14:03:54 +0200

    user registration part 1

--- users/api/urls.py
+++ users/api/urls.py
@@ -10,8 +10,8 @@ app_name = "users"
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name="register"),
-    re_path('registration_password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
-            RegistrationPasswordView.as_view(), name="registration_password"),
+    path('registration_password/<str:uidb64>/<str:token>/',
+         RegistrationPasswordView.as_view(), name="registration_password"),
     path('verify_email/', VerifyEmailView.as_view(), name="verify_email"),
     path('change_password/', ChangePasswordView.as_view(), name="change_password"),
 ]
