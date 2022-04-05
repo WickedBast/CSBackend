6742b61dc5a87c557f1551bfd7d1bdb0d42053ac
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2022-04-05 10:57:21 +0200

    NIP with API KEY

--- CSBackend/views.py
+++ CSBackend/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.utils.translation import gettext as _
 from rest_framework import status
 from rest_framework.response import Response
@@ -9,24 +11,28 @@ class CompanyNIP(APIView):
     permission_classes = []
     authentication_classes = []
 
-    def get(self, request, nip):
-        ID = os.getenv("NIP_ID")
-        KEY = os.getenv("NIP_KEY")
-        nip24 = NIP24Client(id=ID, key=KEY)
-        data = nip24.getAllDataExt(Number.NIP, nip)
+    def get(self, request, nip, token):
+        api_token = os.getenv("API_KEY")
+        if token == api_token:
+            ID = os.getenv("NIP_ID")
+            KEY = os.getenv("NIP_KEY")
+            nip24 = NIP24Client(id=ID, key=KEY)
+            data = nip24.getAllDataExt(Number.NIP, nip)
 
-        if data:
-            response = {
-                "company_name": data.name,
-                "nip": data.nip,
-                "street": data.street,
-                "streetNumber": data.streetNumber,
-                "houseNumber": data.houseNumber,
-                "city": data.city,
-                "postCode": data.postCode,
-                "postCity": data.postCity,
-                "regon": data.regon,
-            }
-            return Response(response, status=status.HTTP_200_OK)
+            if data:
+                response = {
+                    "company_name": data.name,
+                    "nip": data.nip,
+                    "street": data.street,
+                    "streetNumber": data.streetNumber,
+                    "houseNumber": data.houseNumber,
+                    "city": data.city,
+                    "postCode": data.postCode,
+                    "postCity": data.postCity,
+                    "regon": data.regon,
+                }
+                return Response(response, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": _("NIP not found")}, status=status.HTTP_404_NOT_FOUND)
         else:
-            return Response({"error": _("NIP not found")}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": _("Wrong API KEY")}, status=status.HTTP_400_BAD_REQUEST)
