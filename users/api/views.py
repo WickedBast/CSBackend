497a784b24923f84f64ecb5c2d7d7f533fa8f293
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2022-04-16 20:35:41 +0200

    Forgot Password United

--- users/api/views.py
+++ users/api/views.py
@@ -153,28 +153,33 @@ class ChangePasswordView(UpdateAPIView):
 class ForgotPasswordView(ResetPasswordRequestToken):
 
     def post(self, request, *args, **kwargs):
-        current_site = get_current_site(request=request).domain
+        resp = super().post(request=request, *args, **kwargs)
 
-        reset_password_token = ResetPasswordToken.objects.get(
-            user=User.objects.get(email=request.data.get("email")))
+        if resp.status_code == 200:
+            current_site = get_current_site(request=request).domain
 
-        try:
-            requests.post(
-                "https://api.eu.mailgun.net/v3/cleanstock.eu/messages",
-                auth=("api", os.getenv("MAILGUN_API_KEY")),
-                data={"from": "Clean Stock Team <noreply@cleanstock.eu>",
-                      "to": reset_password_token.user.email,
-                      "subject": "Password Reset for Clean Stock Account",
-                      "template": "reset_password",
-                      "v:domain": current_site,
-                      "v:reset_password_url": "{}?token={}".format(
-                          reverse('password_reset:reset-password-confirm'),
-                          reset_password_token.key),
-                      }
-            )
-            return Response({'email': 'Email sent successfully'}, status=status.HTTP_200_OK)
-        except:
-            return Response({'error': 'Email Failed'}, status=status.HTTP_400_BAD_REQUEST)
+            reset_password_token = ResetPasswordToken.objects.get(
+                user=User.objects.get(email=request.data.get("email")))
+
+            try:
+                requests.post(
+                    "https://api.eu.mailgun.net/v3/cleanstock.eu/messages",
+                    auth=("api", os.getenv("MAILGUN_API_KEY")),
+                    data={"from": "Clean Stock Team <noreply@cleanstock.eu>",
+                          "to": reset_password_token.user.email,
+                          "subject": "Password Reset for Clean Stock Account",
+                          "template": "reset_password",
+                          "v:domain": current_site,
+                          "v:reset_password_url": "{}?token={}".format(
+                              reverse('password_reset:reset-password-confirm'),
+                              reset_password_token.key),
+                          }
+                )
+                return Response({'email': 'Email sent successfully'}, status=status.HTTP_200_OK)
+            except:
+                return Response({'error': 'Email Failed'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(self.serializer_class.errors)
 
 
 @method_decorator(csrf_exempt, name="dispatch")
