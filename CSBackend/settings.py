08fdbfe3b00b9f324dece6f006bc2b91e0db5572
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2022-04-20 15:25:33 +0200

    settings updated

--- CSBackend/settings.py
+++ CSBackend/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = [os.getenv("ALLOW_HOST")]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
