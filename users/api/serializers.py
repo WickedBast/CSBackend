715feb6665232f3e96f4fd5d46397ca0067de14d
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2022-04-21 17:30:52 +0200

    Registration part 9

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -29,9 +29,9 @@ class RegistrationSerializer(serializers.Serializer):
     partner_type = serializers.CharField(write_only=True, required=False)
     first_name = serializers.CharField(write_only=True, required=False)
     last_name = serializers.CharField(write_only=True, required=False)
+    taxNumber = serializers.CharField(write_only=True, required=False)
 
     # havePV = serializers.BooleanField(write_only=True, required=False)
-    # taxNumber = serializers.CharField(write_only=True, required=False)
     # technology = serializers.CharField(write_only=True, required=False)
     # installedPeakPVPower = serializers.CharField(write_only=True, required=False)
     # systemLoss = serializers.CharField(write_only=True, required=False)
